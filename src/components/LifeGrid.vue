c853fb440de84482aadb3344b834d762d3dd8fb0
Robert Forster <robertforster@Roberts-Mac-mini.local>
2020-06-13 16:42:19 +0100

    tidy up

--- src/components/LifeGrid.vue
+++ src/components/LifeGrid.vue
@@ -32,15 +32,14 @@
               <v-list-item-title class="headline mb-1">{{ weekDate }}</v-list-item-title>
               <v-list-item-subtitle>to {{ weekEndDate }}</v-list-item-subtitle>
               <v-list-item-subtitle>age: {{ age }}</v-list-item-subtitle>
-              <v-list-item-subtitle>{{ selected }}</v-list-item-subtitle>
+              
             </v-list-item-content>
 
-            <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+            
           </v-list-item>
 
           <v-card-actions>
-            <v-btn text>Button</v-btn>
-            <v-btn text>Button</v-btn>
+            
           </v-card-actions>
         </v-card>
       </v-col>
@@ -239,7 +238,7 @@ export default {
     age: null
   }),
   mounted() {
-    this.items = this.buildItems();    
+    this.items = this.buildItems(this.value);    
   },
   watch: {
     value(val) {
